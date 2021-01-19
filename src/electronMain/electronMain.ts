c190a4b054b2fb32c8a2a75ecce7426145d48bf8
cybershard <ashwatdd01@gmail.com>
2021-01-19 18:49:44 -0500

    Fix launching on windows

--- src/electronMain/electronMain.ts
+++ src/electronMain/electronMain.ts
@@ -91,7 +91,7 @@ export default class ElectronMain {
         if (os.platform() === 'linux' || os.platform() === 'darwin') {
           exec(`wine "${path.join(args.location, 'Among Us.exe')}" --doorstop-enable=${true}`, launchCallback)
         } else {
-          exec(`${path.join(args.location, 'Among Us.exe')} --doorstop-enable=${args.modded}`, launchCallback)
+          exec(`"${path.join(args.location, 'Among Us.exe')}" --doorstop-enable=${args.modded}`, launchCallback)
         }
         break
       case LaunchWrapperType.Custom:
